afc648bc20f6d221a1fa4527b513bfdd9d0c401e
Mecha <mechi13carabajal@gmail.com>
2023-07-13 01:10:21 -0300

    retoques

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import routes from './routes/index.js';
 
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
